5999fbe0ceffdae5125e393f2d8bd729d9c9b201
Aryan Raj <aryann0907@gmail.com>
2026-08-17 23:15:02 +0530

    Add 1832 Leetcode Solution

---
+++ LeetCode/Easy/1832-check-pangram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> check(26,0);
+        for(int i=0;i<sentence.size();i++){
+            int index=sentence[i]-'a';
+            check[index]=1;
+        }
+
+        for(int i=0;i<26;i++){
+            if(check[i]==0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
