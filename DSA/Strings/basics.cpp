08fff1dd3822c6a5e178edc77fd8fec2a9059218
Aryan Raj <aryann0907@gmail.com>
2026-08-24 22:23:32 +0530

    Leetcode Solutions

---
+++ DSA/Strings/basics.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // char arr[]={'a','p','p','l','e'};
+    // cout<<arr<<endl;
+
+    // for(int i=0;i<5;i++){
+    //     cout<<arr[i];
+    // }
+
+    //print only first word
+    // string s;
+    // cin>>s;
+    // cout<<s;
+
+    //print even sentences
+    // string p;
+    // getline(cin,p);
+    // cout<<p;
+
+    // string s1="Aryan";
+    // s1=s1+"ertyui";
+    // cout<<s1;
+    // string s2="Raj";
+    // cout<<s1.size()<<endl;
+    // string s3=s1+s2;
+    // cout<<s3<<endl;
+
+    // s1.push_back('y');
+    // cout<<s1<<endl;
+
+    // s1.pop_back();
+    // cout<<s1<<endl;
+
+    // string p="i am a \"good\" and \\smart bo";
+    // cout<<p;
+
+    // string p="1.1.1.1.1";
+    // string t;
+
+    // for(int i=0;i<p.size();i++){
+    //     if(p[i]=='.'){
+    //         t.append("[.]");
+    //     }
+    //     else{
+    //         t.push_back(p[i]);
+    //     }
+    // }
+    // cout<<t;
+
+    string s;
+    cin>>s;
+    cout<<s;
+
+
+
+}
